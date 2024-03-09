7d87584aead7c19ab4b14cd9d9a0a6c0aa4af57c
Димон <nodilka1@gmail.com>
2024-03-09 14:31:52 +0300

    Update

--- test_add.py
+++ test_add.py
@@ -1,7 +1,15 @@
+import unittest
 from add import add
 
 
-def test_add():
-    assert add(2, 3) == 5
-    assert add(2, 4) == 2
-    assert add(1, -1) == 0
+class TestAddFunction(unittest.TestCase):
+
+    def test1(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test2(self):
+        self.assertEqual(add(1, -1), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
